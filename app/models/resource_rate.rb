2a0aba421f7934f24e36c1ddc4c0006a4e34977f
sean.sullivan <sean.sullivan@beatport.com>
2023-01-06 15:34:25 -0800

    Added rate calculator to model.

--- app/models/resource_rate.rb
+++ app/models/resource_rate.rb
@@ -14,6 +14,34 @@ class ResourceRate < ApplicationRecord
   validates :annual_rate, numericality: { greater_than_or_equal_to: 0 }
 
   # Enumerators
-  enum classification:  [:salary, :contractor, :bonus]
-  enum rate_type: [:hourly, :weekly, :monthly, :annually]
+  enum classification:  [:employee, :contractor, :agency]
+  enum rate_type: [:hourly, :weekly, :monthly, :annual]
+
+  # Callback to calculate rates from the base rate and rate type before
+  # saving the record.
+  before_save :calculate_rates
+
+  # The base rate is used based on the rate type it is.  This method updates
+  # weekly, monthly and annual rates based on the the value of the base rate and
+  # the rate type.
+  def calculate_rates
+    case :rate_type
+    when :hourly
+      weekly_rate = base_rate * 40
+      annual_rate = weekly_rate * 52
+      monthly_rate = annual_rate / 12
+    when :weekly
+      weekly_rate = base_rate
+      annual_rate = weekly_rate * 52
+      monthly_rate = annual_rate / 12
+    when :monthly
+      monthly_rate = base_rate
+      annual_rate = monthly_rate * 12
+      weekly_rate = annual_rate / 52
+    when :annual
+      annual_rate = base_rate
+      weekly_rate = annual_rate / 52
+      monthly_rate = annual_rate / 12
+    end
+  end
 end
